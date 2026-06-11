ce5864c4ebdf1bf9d5c1397d35b52cbe7a4f7ef8
aparicio-ailos <aparicio.silva@ailos.coop.br>
2026-06-10 23:04:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -69,76 +69,12 @@ client.on('authenticated', () => {
   }
 });
 
-client.on('ready', async () => {
-  if (!isReady) {
-    console.log('✅ WhatsApp is ready!');
-    isReady = true;
-
-    const autoDP = process.env.ALWAYS_AUTO_DP || 'False';
-    const autobio = process.env.ALWAYS_AUTO_BIO || 'False';
-    const SHOW_HOROSCOPE = process.env.SHOW_HOROSCOPE || 'False';
-
-    if (SHOW_HOROSCOPE !== 'True' && SHOW_HOROSCOPE !== 'False') {
-      throw new Error(
-        '⚠️ SHOW_HOROSCOPE must be "True" or "False" (as string). Received:',
-        autoDP
-      );
-    }
-
-    if (autoDP === 'True') {
-      if (commands.has('.autodp')) {
-        try {
-          const fakeMessage = {
-            fromMe: true,
-            body: '.autodp',
-            async reply() {},
-          };
-          await commands.get('.autodp').execute(fakeMessage, [], client);
-          console.log('🟢 Enabled AutoDP');
-        } catch (error) {
-          console.error('❌ Failed to enable AutoDP', error);
-        }
-      } else {
-        console.warn('⚠️ .autodp command not found');
-      }
-    } else if (autoDP && autoDP !== 'False') {
-      throw new Error(
-        '⚠️ ALWAYS_AUTO_DP must be "True" or "False" (as string). Received:',
-        autoDP
-      );
-    }
-
-    if (autobio === 'True') {
-      if (commands.has('.autobio')) {
-        try {
-          const fakeMessage = {
-            fromMe: true,
-            body: '.autobio',
-            async reply() {},
-          };
-          await commands.get('.autobio').execute(fakeMessage, [], client);
-          console.log('🟢 Enabled AutoBio');
-        } catch (error) {
-          console.error('❌ Failed to enable AutoBio', error);
-        }
-      } else {
-        console.warn('⚠️ .autobio command not found');
-      }
-    } else if (autobio && autobio !== 'False') {
-      throw new Error(
-        '⚠️ ALWAYS_AUTO_BIO must be "True" or "False" (as string). Received:',
-        autoDP
-      );
-    }
-  }
-});
-
 client.on('auth_failure', message =>
   console.error('❌ Authentication failure:', message)
 );
 const NUMERO_BLOQUEADO = '5547984400667@c.us';
 client.on('message', async message => {
-  console.log(`teste de coomit`);
+  console.log(message.body);
   try {
     if (message.author === NUMERO_BLOQUEADO) {
       await message.delete(false);
@@ -146,27 +82,6 @@ client.on('message', async message => {
     }
   } catch (error) {
     console.error(`❌ Error executing ${command}:`, error);
-  }
-});
-
-// Command handler
-client.on('message_create', async message => {
-  if (
-    !message.fromMe ||
-    typeof message.body !== 'string' ||
-    !message.body.startsWith('.')
-  )
-    return;
-
-  const arguments_ = message.body.trim().split(/\s+/);
-  const command = arguments_.shift().toLowerCase();
-
-  if (commands.has(command)) {
-    try {
-      await commands.get(command).execute(message, arguments_, client);
-    } catch (error) {
-      console.error(`❌ Error executing ${command}:`, error);
-    }
   }
 });
 
